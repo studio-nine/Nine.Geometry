b5c0c9bbd4b27dfd9a864db1211c4b3975be384c
Eric Tuvesson <eric.tuvesson@gmail.com>
2017-02-18 13:28:42 +0100

    Add .NET Standard Project, add simple Point and Rectangle structs

---
+++ src/Nine.Geometry/Point.cs
@@ -0,0 +1,71 @@
+﻿namespace Nine.Geometry
+{
+    using System;
+    using System.Numerics;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public struct Point : IEquatable<Point>
+    {
+        public int X;
+        public int Y;
+
+        /// <summary>
+        /// Initialize a new <see cref="Point"/> struct.
+        /// </summary>
+        /// <param name="value"></param>
+        public Point(int value)
+            : this(value, value)
+        {
+
+        }
+
+        /// <summary>
+        /// Initialize a new <see cref="Point"/> struct.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        /// <summary>
+        /// Convert the <see cref="Point"/> to a <see cref="Vector2"/>.
+        /// </summary>
+        public Vector2 ToVector2()
+        {
+            return new Vector2(X, Y);
+        }
+
+        public static Point operator +(Point value1, Point value2) => new Point(value1.X + value2.X, value1.Y + value2.Y);
+        public static Point operator -(Point value1, Point value2) => new Point(value1.X - value2.X, value1.Y - value2.Y);
+        public static Point operator *(Point value1, Point value2) => new Point(value1.X * value2.X, value1.Y * value2.Y);
+        public static Point operator /(Point value1, Point value2) => new Point(value1.X / value2.X, value1.Y / value2.Y);
+
+        public static bool operator ==(Point a, Point b) => a.Equals(b);
+        public static bool operator !=(Point a, Point b) => !a.Equals(b);
+
+        public bool Equals(Point other) => ((X == other.X) && (Y == other.Y));
+
+        public override bool Equals(object obj)
+        {
+            return (obj is Point) && Equals((Point)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (X * 397) ^ Y;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"X: {X}, Y: {Y}";
+        }
+    }
+}
---
+++ src/Nine.Geometry/Rectangle.cs
@@ -0,0 +1,97 @@
+﻿namespace Nine.Geometry
+{
+    using System;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public struct Rectangle : IEquatable<Rectangle>
+    {
+        public int X;
+        public int Y;
+        public int Width;
+        public int Height;
+
+        /// <summary>
+        /// Gets the left side of the rectangle.
+        /// </summary>
+        public int Left => X;
+
+        /// <summary>
+        /// Gets the right side of the rectangle.
+        /// </summary>
+        public int Right => X + Width;
+
+        /// <summary>
+        /// Gets the top side of the rectangle.
+        /// </summary>
+        public int Top => Y;
+
+        /// <summary>
+        /// Gets the bottom side of the rectangle.
+        /// </summary>
+        public int Bottom => Y + Height;
+
+        /// <summary>
+        /// Initialize a new <see cref="Rectangle"/> struct.
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="size"></param>
+        public Rectangle(Point position, Point size)
+            : this(position.X, position.Y, size.X, size.Y)
+        {
+
+        }
+
+        /// <summary>
+        /// Initialize a new <see cref="Rectangle"/> struct.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        public Rectangle(int x, int y, int width, int height)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public bool Contains(Point value) => Contains(value.X, value.Y);
+        public bool Contains(int x, int y)
+        {
+            return (this.X <= x) && x < (this.X + this.Width) 
+                && (this.Y <= y) && y < (this.Y + this.Height);
+        }
+
+        public bool Intersects(Rectangle value)
+        {
+            return value.Left < Right && Left < value.Right &&
+                   value.Top < Bottom && Top < value.Bottom;
+        }
+
+        public static bool operator ==(Rectangle a, Rectangle b) => ((a.X == b.X) && (a.Y == b.Y) && (a.Width == b.Width) && (a.Height == b.Height));
+        public static bool operator !=(Rectangle a, Rectangle b) => !(a == b);
+
+        public bool Equals(Rectangle other)
+        {
+            return this == other;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return (obj is Rectangle) && this == ((Rectangle)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{X}, {Y}, {Width}, {Height}";
+        }
+    }
+}
