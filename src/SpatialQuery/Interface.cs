97069fbc57c641945b42f1c2df6340f47ab5cbf2
Eric Tuvesson <eric.tuvesson@gmail.com>
2021-05-16 12:29:54 +0200

    feat: Add QuadTree and Octree
    This code is taken from Engine Nine
    https://github.com/studio-nine/Engine-Nine
    
    Co-Authored-By: Yufei Huang <yufeih@live.com>

---
+++ src/SpatialQuery/Interface.cs
@@ -0,0 +1,75 @@
+﻿namespace Nine.Geometry.SpatialQuery
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Defines Spatial relations between objects.
+    /// </summary>
+    public interface ISpatialQuery
+    {
+        /// <summary>
+        /// Creates a spatial query of the specified target type.
+        /// </summary>
+        ISpatialQuery<T> CreateSpatialQuery<T>(Predicate<T> condition) where T : class;
+    }
+
+    /// <summary>
+    /// Defines Spatial relations between objects.
+    /// </summary>
+    public interface ISpatialQuery<T>
+    {
+        /// <summary>
+        /// Finds all the objects that intersects with the specified ray.
+        /// </summary>
+        /// <param name="result">The caller is responsible for clearing the result collection</param>
+        void FindAll(ref Ray ray, ICollection<T> result);
+
+        /// <summary>
+        /// Finds all the objects resides within the specified bounding sphere.
+        /// </summary>
+        /// <param name="result">The caller is responsible for clearing the result collection</param>
+        void FindAll(ref BoundingSphere boundingSphere, ICollection<T> result);
+
+        /// <summary>
+        /// Finds all the objects that intersects with the specified bounding box.
+        /// </summary>
+        /// <param name="result">The caller is responsible for clearing the result collection</param>
+        void FindAll(ref BoundingBox boundingBox, ICollection<T> result);
+
+        /// <summary>
+        /// Finds all the objects resides within the specified bounding frustum.
+        /// </summary>
+        /// <param name="result">The caller is responsible for clearing the result collection</param>
+        void FindAll(BoundingFrustum boundingFrustum, ICollection<T> result);
+    }
+
+    /// <summary>
+    /// Interface for an object that can be queried by a scene manager.
+    /// </summary>
+    public interface ISpatialQueryable
+    {
+        /// <summary>
+        /// Gets the axis aligned bounding box of this instance in world space.
+        /// </summary>
+        BoundingBox BoundingBox { get; }
+
+        /// <summary>
+        /// Occurs when the bounding box changed.
+        /// </summary>
+        event EventHandler<EventArgs> BoundingBoxChanged;
+
+        /// <summary>
+        /// Gets or sets the data used for spatial query.
+        /// </summary>
+        object SpatialData { get; set; }
+    }
+
+    /// <summary>
+    /// Interface for a scene manager that manages the spatial relationships
+    /// between objects.
+    /// </summary>
+    public interface ISceneManager<T> : ICollection<T>, ISpatialQuery<T>
+    {
+    }
+}
---
+++ test/SpatialQuery/QuadTreeTest.cs
@@ -0,0 +1,37 @@
+﻿namespace Nine.Geometry.Test.SpatialQuery
+{
+    using Nine.Geometry.SpatialQuery;
+    using System.Numerics;
+    using Xunit;
+
+    public class QuadTreeTest
+    {
+        [Fact]
+        public void AddRemoveTest()
+        {
+            Octree<bool> oct = new Octree<bool>(new BoundingBox(Vector3.Zero, Vector3.One * 4), 2);
+
+            oct.ExpandAll((o) => { return o.Bounds.Contains(Vector3.One * 0.1f) == ContainmentType.Contains; });
+
+            object a = new object();
+            object b = new object();
+
+            QuadTree<object> tree = new QuadTree<object>(new BoundingRectangle(4, 4), 2);
+        }
+
+        [Fact]
+        public void ExpandTest()
+        {
+            Assert.Equal(ContainmentType.Contains,
+                new BoundingBox(Vector3.Zero, Vector3.One).Contains(Vector3.One));
+
+            BoundingRectangle bounds = new BoundingRectangle(4, 4);
+
+            QuadTree<object> tree = new QuadTree<object>(bounds, 2);
+
+            Assert.Equal(bounds, tree.Bounds);
+            Assert.Equal(bounds, tree.Root.Bounds);
+            Assert.Equal(0, tree.Root.Depth);
+        }
+    }
+}
