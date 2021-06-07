5413a997c374372c5554c9be2bc2ca71270df898
Eric Tuvesson <eric.tuvesson@gmail.com>
2021-06-08 00:32:24 +0200

    feat: Add GeometryHelper

---
+++ src/GeometryHelper.cs
@@ -0,0 +1,44 @@
+﻿namespace Nine.Geometry
+{
+    using System.Numerics;
+    
+    public static class GeometryHelper
+    {
+        /// <summary>
+        /// Returns whether the points are in counter clockwise order.
+        /// </summary>
+        /// <param name="points"></param>
+        /// <returns></returns>
+        public static bool PointsAreCounterClockwiseOrder(Vector2[] points)
+        {
+            float signedArea = 0;
+            for (int i = 0; i < points.Length; i++)
+            {
+                int nextIndex = (i + 1) % points.Length;
+                signedArea += (points[nextIndex].X - points[i].X)
+                            * (points[nextIndex].Y + points[i].Y);
+            }
+
+            return signedArea < 0;
+        }
+
+        /// <summary>
+        /// Returns whether the points are in counter clockwise order.
+        /// </summary>
+        /// <param name="points"></param>
+        /// <returns></returns>
+        public static bool PointsAreCounterClockwiseOrder(Vector3[] points)
+        {
+            float signedArea = 0;
+            for (int i = 0; i < points.Length; i++)
+            {
+                int nextIndex = (i + 1) % points.Length;
+                signedArea += (points[nextIndex].X - points[i].X)
+                            * (points[nextIndex].Y + points[i].Y)
+                            * (points[nextIndex].Z + points[i].Z);
+            }
+
+            return signedArea < 0;
+        }
+    }
+}
---
+++ test/GeometryHelperTest.cs
@@ -0,0 +1,68 @@
+﻿namespace Nine.Geometry.Test
+{
+    using System.Numerics;
+    using Xunit;
+    
+    public class GeometryHelperTest
+    {
+        [Fact]
+        public void PointsAreCounterClockwiseOrder_Vector2()
+        {
+            var points = new Vector2[]
+            {
+                new Vector2(0, 0),
+                new Vector2(1, 0),
+                new Vector2(1, 1),
+            };
+
+            var counterClockwise = GeometryHelper.PointsAreCounterClockwiseOrder(points);
+
+            Assert.True(counterClockwise);
+        }
+
+        [Fact]
+        public void PointsAreCounterClockwiseOrder_Vector2_Clockwise()
+        {
+            var points = new Vector2[]
+            {
+                new Vector2(1, 1),
+                new Vector2(1, 0),
+                new Vector2(0, 0),
+            };
+
+            var counterClockwise = GeometryHelper.PointsAreCounterClockwiseOrder(points);
+
+            Assert.False(counterClockwise);
+        }
+
+        [Fact]
+        public void PointsAreCounterClockwiseOrder_Vector3()
+        {
+            var points = new Vector3[]
+            {
+                new Vector3(1, 1, 1),
+                new Vector3(1, 0, 1),
+                new Vector3(0, 0, 1),
+            };
+
+            var counterClockwise = GeometryHelper.PointsAreCounterClockwiseOrder(points);
+
+            Assert.False(counterClockwise);
+        }
+
+        [Fact]
+        public void PointsAreCounterClockwiseOrder_Vector3_Clockwise()
+        {
+            var points = new Vector3[]
+            {
+                new Vector3(1, 1, 1),
+                new Vector3(1, 0, 1),
+                new Vector3(0, 0, 1),
+            };
+
+            var counterClockwise = GeometryHelper.PointsAreCounterClockwiseOrder(points);
+
+            Assert.False(counterClockwise);
+        }
+    }
+}
